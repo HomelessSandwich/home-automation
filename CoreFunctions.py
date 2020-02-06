6d6aa172c9750c74bb85a8f82051144cc07f4efc
Oliver Wilkins <okwilkins@googlemail.com>
2020-02-06 20:48:05 +0000

    Added ip send email + core functions for database interaction

--- CoreFunctions.py
+++ CoreFunctions.py
@@ -1,5 +1,7 @@
 import smtplib
 import requests
+import pandas as pd
+from sqlalchemy import create_engine
 from email.mime.text import MIMEText
 
 
@@ -24,6 +26,79 @@ class Email():
 
     def close_connection(self):
         self.smtp.quit()
+
+
+class DatabaseConnection:
+    def __init__(self, host, port, user, pwd, db):
+        try:
+            print(f"\nAttemping to connect to server: {user}@{host}:{port}/{db}")
+            conn_str = f'postgresql://{user}:{pwd}@{host}:{port}/{db}'
+            self.engine = create_engine(conn_str)
+
+        except Exception as e:
+            print("Cannot connect to the database!")
+            print(e)
+            print("\nDatabase connection closed.")
+        else:
+            print("Connection successful!")
+
+    def get_table_dataframe(self, table_name):
+        """
+        Puts a table from a database into a pandas data frame.
+        Inputs:
+            table_name (str): name of the table from the database
+        Returns: pandas dataframe
+        """
+        print(f"\nAttempting to read table: {table_name}")
+
+        try:
+            # Read table from database
+            # fillna to avoid casting issues of integer cols that have NULLS
+            df = pd.read_sql(f"SELECT * FROM {table_name}", con=self.engine)
+            df = df.where((pd.notnull(df)), None)
+        except Exception as e:
+            print("Could not get table and put into pandas dataframe!")
+            print(e + "\n")
+            # Return empty dataframe
+            df = pd.DataFrame()
+        else:
+            print(f"Reading successful: {table_name}")
+        finally:
+            return df
+
+    def execute(self, cmd=''):
+        '''
+        Executes an SQL command.
+
+        cmd (str): The SQL command to be executed.
+        '''
+
+        result = self.engine.execute(cmd)
+        return result
+
+    def write_to_table(self, table_name, schema, df):
+        '''
+        Writes Pandas dataframe to a table.
+
+        table_name (str): The name of the table to be written to.
+        schema (str): The name of the schema, containing the table to be written to.
+        df (pandas.DataFrame): The dataframe containing the data to write from.
+        '''
+
+        print('\nAttempting to write to:')
+        print(f'\tSchema: {schema} | Table: {table_name}')
+
+        try:
+            df.to_sql(
+                name=table_name, schema=schema, con=self.engine,
+                if_exists='append', index=False
+            )
+        except Exception as e:
+            print("Could not write dataframe to table!")
+            print(e + "\n")
+        else:
+            print('Sucessful to write to:')
+            print(f'\tSchema: {schema} | Table: {table_name}')
 
 
 def get_ip():
--- send-public-ip.py
+++ send-public-ip.py
@@ -4,8 +4,10 @@ Oliver Wilkins: 2020-02-06
 This script sends a device's public IP address to a given email address.
 '''
 
-from CoreFunctions import Email, get_ip
+from CoreFunctions import Email, DatabaseConnection, get_ip
 import json
+import pandas as pd
+from datetime import datetime
 
 
 def get_email_pwd():
@@ -18,6 +20,49 @@ def get_email_pwd():
     return data
 
 
+def get_db_details():
+    db_file_path = '.private/db.json'
+
+    with open(db_file_path, 'r') as json_file:
+        data = json.loads(json_file.read())
+
+    return data
+
+
+def check_ip_changed(db_ip, db_port, db_user, db_pwd, db_name, table_name, schema, ip):
+
+    db = DatabaseConnection(
+        host=db_ip,
+        port=db_port,
+        user=db_user,
+        pwd=db_pwd,
+        db=db_name,
+    )
+
+    df_ips = db.get_table_dataframe(table_name)
+
+    if not df_ips.empty:
+        # Get the IP address that was last used
+        last_ip = df_ips.iloc[df_ips['time_detected'].idxmax()]['ip']
+
+    if (ip not in df_ips['ip'].unique()) or (ip != last_ip):
+        # if current IP is not in stored IPs OR
+        # current IP does not equal the last IP used
+        # write current IP
+
+        df = pd.DataFrame(
+            data=[[ip, str(datetime.now())]],
+            columns=['ip', 'time_detected']
+        )
+
+        db.write_to_table(schema=schema, table_name=table_name, df=df)
+        return True
+    else:
+        print('\nIP address already exists in table!')
+        print('No action needed.')
+        return False
+
+
 def send_ip(email, pwd, ip):
 
     emailer = Email(
@@ -27,7 +72,7 @@ def send_ip(email, pwd, ip):
 
     body = 'Current Public IP Address: ' + ip
 
-    print('Sending public IP address to: ' + email + '...')
+    print('\nSending public IP address to: ' + email + '...')
 
     emailer.send(
         to_email=email,
@@ -40,10 +85,19 @@ def send_ip(email, pwd, ip):
 
 if __name__ == '__main__':
     email_pwd = get_email_pwd()
+    db_details = get_db_details()
     ip = get_ip()
 
-    send_ip(
-        email=email_pwd['email'],
-        pwd=email_pwd['password'],
-        ip=ip
+    ip_changed = check_ip_changed(
+        db_ip=db_details['host'], db_port=db_details['port'],
+        db_user=db_details['user'], db_pwd=db_details['password'],
+        db_name=db_details['database'], schema=db_details['schema'],
+        table_name=db_details['table'], ip=ip
     )
+
+    if ip_changed:
+        send_ip(
+            email=email_pwd['email'],
+            pwd=email_pwd['password'],
+            ip=ip
+        )
